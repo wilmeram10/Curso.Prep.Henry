0d8f6d1e98250b76f6f70f42c5c822b5d39101d7
unknown <Wilmer>
2021-05-25 19:09:53 -0500

    Homework number two JavaScript, Updated.

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -202,7 +202,7 @@ function esVerdadero(valor){
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí.
+  //Escribe tu código aquí
   var tabla = []; 
   for(var i=0; i<=10; i++){
   	tabla [i]= 6 * i;
