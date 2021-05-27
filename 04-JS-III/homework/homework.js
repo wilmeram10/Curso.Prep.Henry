96ace870b400fbf684439ef145d00ce684c814d8
unknown <Wilmer>
2021-05-27 15:25:34 -0500

    Homework Number three JavaScript, Updated.

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -36,7 +36,7 @@ function incrementarPorUno(array) {
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
-  // y devuelve el array
+  // y devuelve el array.
   // Tu código:
   array.push(elemento);
   return array;
